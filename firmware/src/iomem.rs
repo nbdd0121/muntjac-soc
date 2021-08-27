de51018d34ef2b081ca5353ad34dbfef4a712368
Gary Guo <gary@garyguo.net>
2021-08-27 12:40:54 +0100

    IoMem 64-bit read

--- firmware/src/iomem.rs
+++ firmware/src/iomem.rs
@@ -1,6 +1,7 @@
 use core::mem;
 use core::ptr;
 
+#[derive(Clone, Copy)]
 pub struct IoMem<const SIZE: usize> {
     ptr: usize,
 }
@@ -34,6 +35,11 @@ impl<const SIZE: usize> IoMem<SIZE> {
     }
 
     #[inline]
+    pub fn read_u64(&self, offset: usize) -> u64 {
+        self.read(offset)
+    }
+
+    #[inline]
     pub fn write<T: Copy>(&self, offset: usize, value: T) {
         assert!(offset + mem::size_of::<T>() <= SIZE);
         assert!(offset % mem::align_of::<T>() == 0);
@@ -52,6 +58,11 @@ impl<const SIZE: usize> IoMem<SIZE> {
 
     #[inline]
     pub fn write_u32(&self, offset: usize, value: u32) {
+        self.write(offset, value)
+    }
+
+    #[inline]
+    pub fn write_u64(&self, offset: usize, value: u64) {
         self.write(offset, value)
     }
 }
