b2bbd9c280b60d5a64b13785b45b01a495d50633
Gary Guo <gary@garyguo.net>
2021-11-14 22:01:20 +0000

    Add genesys 2 support

---
+++ rtl/chip_top_genesys_2.sv
@@ -0,0 +1,379 @@
+`include "tl_util.svh"
+
+module chip_top (
+  input  sys_clk_p,
+  input  sys_clk_n,
+  input  sys_rst_ni,
+  // QSPI
+  inout  [1:0] qspi_dq,
+  output       qspi_csn,
+  // UART
+  input  uart_rx,
+  output uart_tx,
+  // SD
+  input        sd_cd,
+  inout        sd_cmd,
+  inout  [3:0] sd_dat,
+  output       sd_reset,
+  output       sd_sck,
+  // DDR
+  output [14:0] ddr3_sdram_addr,
+  output [2:0]  ddr3_sdram_ba,
+  output        ddr3_sdram_cas_n,
+  output [0:0]  ddr3_sdram_ck_n,
+  output [0:0]  ddr3_sdram_ck_p,
+  output [0:0]  ddr3_sdram_cke,
+  output [0:0]  ddr3_sdram_cs_n,
+  output [3:0]  ddr3_sdram_dm,
+  inout  [31:0] ddr3_sdram_dq,
+  inout  [3:0]  ddr3_sdram_dqs_n,
+  inout  [3:0]  ddr3_sdram_dqs_p,
+  output [0:0]  ddr3_sdram_odt,
+  output        ddr3_sdram_ras_n,
+  output        ddr3_sdram_reset_n,
+  output        ddr3_sdram_we_n
+);
+
+  localparam NumCores = 2;
+  localparam TimerClkFreq = 50;
+  localparam AddrWidth = 38;
+  localparam DmaSourceWidth = 3;
+  localparam HostSourceWidth = 2;
+  localparam DeviceSourceWidth = 5;
+  localparam SinkWidth = 4;
+
+  logic clk;
+  logic io_clk;
+  logic rstn;
+
+  `TL_DECLARE(128, AddrWidth, DmaSourceWidth, SinkWidth, dma_tl);
+  `TL_DECLARE(128, AddrWidth, DeviceSourceWidth, 1, mem_tl);
+  `TL_DECLARE(32, AddrWidth, DeviceSourceWidth, 1, rom_tl);
+  `TL_DECLARE(64, AddrWidth, DeviceSourceWidth, 1, io_tl);
+
+  /////////////////
+  // #region DMA //
+
+  // No IO devices can perform DMA just yet.
+  assign dma_tl_a_valid = 1'b0;
+  assign dma_tl_a = 'x;
+  assign dma_tl_b_ready = 1'b0;
+  assign dma_tl_c_valid = 1'b0;
+  assign dma_tl_c = 'x;
+  assign dma_tl_d_ready = 1'b0;
+  assign dma_tl_e_valid = 1'b0;
+  assign dma_tl_e = 'x;
+
+  // #endregion
+  /////////////////
+
+  /////////////////
+  // #region CCX //
+
+  logic [NumCores-1:0] irq_timer_m;
+  logic [NumCores-1:0] irq_software_m;
+  logic [NumCores-1:0] irq_external_s;
+  logic [NumCores-1:0] irq_external_m;
+
+  ccx #(
+    .NumCores (NumCores),
+    .DmaSourceWidth (DmaSourceWidth),
+    .DeviceSourceWidth (DeviceSourceWidth),
+    .SinkWidth (SinkWidth),
+    .AddrWidth (AddrWidth)
+  ) ccx (
+    .clk_i (clk),
+    .rst_ni (rstn),
+    .irq_software_m_i (irq_software_m),
+    .irq_timer_m_i (irq_timer_m),
+    .irq_external_m_i (irq_external_m),
+    .irq_external_s_i (irq_external_s),
+    `TL_CONNECT_DEVICE_PORT(dma, dma_tl),
+    `TL_CONNECT_HOST_PORT(mem, mem_tl),
+    `TL_CONNECT_HOST_PORT(rom, rom_tl),
+    `TL_CONNECT_HOST_PORT(io, io_tl)
+  );
+
+  // #endregion
+  /////////////////
+
+  ///////////////////////////
+  // #region DDR and clock //
+
+  `TL_DECLARE(128, 30, 5, 1, ddr_tl);
+
+  ddr #(
+    .DataWidth   (128),
+    .AddrWidth   (30),
+    .SourceWidth (5)
+  ) ddr (
+    .sys_clk_p,
+    .sys_clk_n,
+    .sys_rst_ni,
+    .clk_o    (clk),
+    .io_clk_o (io_clk),
+    .rst_no   (rstn),
+    .ddr3_sdram_addr,
+    .ddr3_sdram_ba,
+    .ddr3_sdram_cas_n,
+    .ddr3_sdram_ck_n,
+    .ddr3_sdram_ck_p,
+    .ddr3_sdram_cke,
+    .ddr3_sdram_cs_n,
+    .ddr3_sdram_dm,
+    .ddr3_sdram_dq,
+    .ddr3_sdram_dqs_n,
+    .ddr3_sdram_dqs_p,
+    .ddr3_sdram_odt,
+    .ddr3_sdram_ras_n,
+    .ddr3_sdram_reset_n,
+    .ddr3_sdram_we_n,
+    `TL_CONNECT_DEVICE_PORT(link, ddr_tl)
+  );
+
+  tl_adapter #(
+    .HostDataWidth (128),
+    .DeviceDataWidth (128),
+    .HostAddrWidth (AddrWidth),
+    .DeviceAddrWidth (30),
+    .HostSourceWidth (DeviceSourceWidth),
+    .DeviceSourceWidth (5),
+    .HostSinkWidth (1),
+    .DeviceSinkWidth (1),
+    .HostMaxSize (6),
+    .DeviceMaxSize (6),
+    .HostFifo (1'b0),
+    .DeviceFifo (1'b0)
+  ) mem_adapter (
+    .clk_i (clk),
+    .rst_ni (rstn),
+    `TL_CONNECT_DEVICE_PORT(host, mem_tl),
+    `TL_CONNECT_HOST_PORT(device, ddr_tl)
+  );
+
+  // #endregion
+  ///////////////////////////
+
+  ///////////////////////
+  // #region SPI flash //
+
+  `TL_DECLARE(32, 25, 1, 1, flash_tl);
+
+  spi #(
+    .DataWidth   (32),
+    .AddrWidth   (25),
+    .SourceWidth (1)
+  ) spi_flash (
+    .clk_i (clk),
+    .rst_ni (rstn),
+    .io_clk_i (io_clk),
+    .spi_io_io (qspi_dq[1:0]),
+    .spi_ss_io (qspi_csn),
+    `TL_CONNECT_DEVICE_PORT(link, flash_tl)
+  );
+
+  tl_adapter #(
+    .HostDataWidth (32),
+    .DeviceDataWidth (32),
+    .HostAddrWidth (AddrWidth),
+    .DeviceAddrWidth (25),
+    .HostSourceWidth (DeviceSourceWidth),
+    .DeviceSourceWidth (1),
+    .HostSinkWidth (1),
+    .DeviceSinkWidth (1),
+    .HostMaxSize (6),
+    .DeviceMaxSize (6),
+    .HostFifo (1'b0),
+    .DeviceFifo (1'b0)
+  ) rom_adapter (
+    .clk_i (clk),
+    .rst_ni (rstn),
+    `TL_CONNECT_DEVICE_PORT(host, rom_tl),
+    `TL_CONNECT_HOST_PORT(device, flash_tl)
+  );
+
+  // #endregion
+  ///////////////////////
+
+  ///////////////////////
+  // #region IO Switch //
+
+  `TL_DECLARE_ARR(64, AddrWidth, DeviceSourceWidth, 1, io_ch, [3:0]);
+
+  localparam [AddrWidth-1:0] PlicBaseAddr  = 'h11000000;
+  localparam [AddrWidth-1:0] PlicBaseMask  = 'h  3FFFFF;
+
+  localparam [AddrWidth-1:0] ClintBaseAddr = 'h11400000;
+  localparam [AddrWidth-1:0] ClintBaseMask = 'h    FFFF;
+
+  localparam [AddrWidth-1:0] SdhciBaseAddr = 'h10010000;
+  localparam [AddrWidth-1:0] SdhciBaseMask = 'h     FFF;
+
+  tl_socket_1n #(
+    .SourceWidth (DeviceSourceWidth),
+    .AddrWidth (AddrWidth),
+    .DataWidth (64),
+    .NumLinks    (4),
+    .NumAddressRange (3),
+    .AddressBase ({ClintBaseAddr, PlicBaseAddr, SdhciBaseAddr}),
+    .AddressMask ({ClintBaseMask, PlicBaseMask, SdhciBaseMask}),
+    .AddressLink ({3'd         1, 3'd        2, 3'd         3})
+  ) io_socket_1n (
+    .clk_i (clk),
+    .rst_ni (rstn),
+    `TL_CONNECT_DEVICE_PORT(host, io_tl),
+    `TL_CONNECT_HOST_PORT(device, io_ch)
+  );
+
+  // #endregion
+  ///////////////////////
+
+  ////////////////////////////
+  // #region PLIC and CLINT //
+
+  clint_tl #(
+    .NumHarts (NumCores),
+    .TimerClockFrequency (50),
+    .AddrWidth (AddrWidth),
+    .SourceWidth (DeviceSourceWidth)
+  ) clint (
+    .clk_i (clk),
+    .rst_ni (rstn),
+    .timer_clk_i (io_clk),
+    .msip_o (irq_software_m),
+    .mtip_o (irq_timer_m),
+    `TL_CONNECT_DEVICE_PORT_IDX(link, io_ch, [1])
+  );
+
+  logic [31:0] interrupts;
+  logic [31:0] edge_trigger;
+
+  plic_tl #(
+    .NumContexts (NumCores),
+    .NumIrqs (32),
+    .AddrWidth (AddrWidth),
+    .SourceWidth (DeviceSourceWidth)
+  ) plic (
+    .clk_i (clk),
+    .rst_ni (rstn),
+    .interrupts_i (interrupts),
+    .edge_trigger_i (edge_trigger),
+    .irq_o (irq_external_s),
+    `TL_CONNECT_DEVICE_PORT_IDX(link, io_ch, [2])
+  );
+
+  // #endregion
+  ////////////////////////////
+
+  //////////////////
+  // #region UART //
+
+  `TL_DECLARE(32, 13, 1, 1, uart_tl);
+
+  logic irq_uart;
+
+  uart #(
+    .DataWidth   (32),
+    .AddrWidth   (13),
+    .SourceWidth (1)
+  ) uart (
+    .clk_i (clk),
+    .rst_ni (rstn),
+    .io_clk_i (io_clk),
+    .uart_rts (1'b0),
+    .uart_cts (),
+    .uart_txd (uart_rx),
+    .uart_rxd (uart_tx),
+    `TL_CONNECT_DEVICE_PORT(link, uart_tl),
+    .irq_o (irq_uart)
+  );
+  
+  tl_adapter #(
+    .HostDataWidth (64),
+    .DeviceDataWidth (32),
+    .HostAddrWidth (AddrWidth),
+    .DeviceAddrWidth (13),
+    .HostSourceWidth (DeviceSourceWidth),
+    .DeviceSourceWidth (1),
+    .HostSinkWidth (1),
+    .DeviceSinkWidth (1),
+    .HostMaxSize (3),
+    .DeviceMaxSize (2),
+    .HostFifo (1'b0),
+    .DeviceFifo (1'b1)
+  ) uart_adapter (
+    .clk_i (clk),
+    .rst_ni (rstn),
+    `TL_CONNECT_DEVICE_PORT_IDX(host, io_ch, [0]),
+    `TL_CONNECT_HOST_PORT(device, uart_tl)
+  );
+
+  // #endregion
+  //////////////////
+  
+  ///////////////////
+  // #region SDHCI //
+
+  `TL_DECLARE(32, 12, 1, 1, sdhci_tl);
+
+  logic irq_sd;
+
+  sdhci #(
+    .AddrWidth   (12),
+    .SourceWidth (1)
+  ) sdhci (
+    .clk_i (clk),
+    .rst_ni (rstn),
+    .io_clk_i (io_clk),
+    .sd_cd,
+    .sd_cmd,
+    .sd_dat,
+    .sd_reset,
+    .sd_sck,
+    `TL_CONNECT_DEVICE_PORT(link, sdhci_tl),
+    .irq_o (irq_sd)
+  );
+  
+  tl_adapter #(
+    .HostDataWidth (64),
+    .DeviceDataWidth (32),
+    .HostAddrWidth (AddrWidth),
+    .DeviceAddrWidth (12),
+    .HostSourceWidth (DeviceSourceWidth),
+    .DeviceSourceWidth (1),
+    .HostSinkWidth (1),
+    .DeviceSinkWidth (1),
+    .HostMaxSize (3),
+    .DeviceMaxSize (2),
+    .HostFifo (1'b0),
+    .DeviceFifo (1'b1)
+  ) sdhci_adapter (
+    .clk_i (clk),
+    .rst_ni (rstn),
+    `TL_CONNECT_DEVICE_PORT_IDX(host, io_ch, [3]),
+    `TL_CONNECT_HOST_PORT(device, sdhci_tl)
+  );
+
+  // #endregion
+  ///////////////////
+
+  ////////////////////////
+  // region IRQ routing //
+
+  always_comb begin
+    interrupts = '0;
+    edge_trigger = '0;
+
+    // UART IRQ is level-triggered
+    interrupts[1] = irq_uart;
+    edge_trigger[1] = 1'b0;
+
+    // SD IRQ is level-triggered
+    interrupts[2] = irq_sd;
+    edge_trigger[1] = 1'b0;
+  end
+
+  // endregion
+  ////////////////////////
+
+endmodule
---
+++ rtl/ddr_genesys_2.sv
@@ -0,0 +1,226 @@
+`include "axi_util.svh"
+`include "tl_util.svh"
+
+module ddr #(
+  parameter DataWidth   = 128,
+  parameter AddrWidth   = 30,
+  parameter SourceWidth = 5
+) (
+  // Clock and reset
+  input  sys_clk_p,
+  input  sys_clk_n,
+  input  sys_rst_ni,
+
+  output clk_o,
+  output io_clk_o,
+  output rst_no,
+
+  // IO port
+  output [14:0] ddr3_sdram_addr,
+  output [2:0]  ddr3_sdram_ba,
+  output        ddr3_sdram_cas_n,
+  output [0:0]  ddr3_sdram_ck_n,
+  output [0:0]  ddr3_sdram_ck_p,
+  output [0:0]  ddr3_sdram_cke,
+  output [0:0]  ddr3_sdram_cs_n,
+  output [3:0]  ddr3_sdram_dm,
+  inout  [31:0] ddr3_sdram_dq,
+  inout  [3:0]  ddr3_sdram_dqs_n,
+  inout  [3:0]  ddr3_sdram_dqs_p,
+  output [0:0]  ddr3_sdram_odt,
+  output        ddr3_sdram_ras_n,
+  output        ddr3_sdram_reset_n,
+  output        ddr3_sdram_we_n,
+
+  // TileLink port
+  `TL_DECLARE_DEVICE_PORT(DataWidth, AddrWidth, SourceWidth, 1, link)
+);
+
+  `AXI_DECLARE(DataWidth, AddrWidth, SourceWidth, axi);
+
+  wire mig_clk;
+
+  mig_7series_0 ddr_ctrl (
+    // Clock
+    .sys_clk_p       (sys_clk_p),
+    .sys_clk_n       (sys_clk_n),
+    .sys_rst         (sys_rst_ni),
+    .ui_clk          (mig_clk),
+    .ui_clk_sync_rst (),
+    .ui_addn_clk_0   (clk_o),
+    .ui_addn_clk_1   (io_clk_o),
+    .ui_addn_clk_2   (),
+    .ui_addn_clk_3   (),
+    .ui_addn_clk_4   (),
+    .mmcm_locked     (rst_no),
+    // DDR interface
+    .ddr3_addr   (ddr3_sdram_addr),
+    .ddr3_ba     (ddr3_sdram_ba),
+    .ddr3_cas_n  (ddr3_sdram_cas_n),
+    .ddr3_ck_n   (ddr3_sdram_ck_n),
+    .ddr3_ck_p   (ddr3_sdram_ck_p),
+    .ddr3_cke    (ddr3_sdram_cke),
+    .ddr3_cs_n   (ddr3_sdram_cs_n),
+    .ddr3_dm     (ddr3_sdram_dm),
+    .ddr3_dq     (ddr3_sdram_dq),
+    .ddr3_dqs_n  (ddr3_sdram_dqs_n),
+    .ddr3_dqs_p  (ddr3_sdram_dqs_p),
+    .ddr3_odt    (ddr3_sdram_odt),
+    .ddr3_ras_n  (ddr3_sdram_ras_n),
+    .ddr3_reset_n (ddr3_sdram_reset_n),
+    .ddr3_we_n   (ddr3_sdram_we_n),
+    // AXI interface
+    .aresetn       (rst_no),
+    .s_axi_arready (axi_ar_ready),
+    .s_axi_arvalid (axi_ar_valid),
+    .s_axi_araddr  (axi_ar.addr),
+    .s_axi_arburst (axi_ar.burst),
+    .s_axi_arcache (axi_ar.cache),
+    .s_axi_arid    (axi_ar.id),
+    .s_axi_arlen   (axi_ar.len),
+    .s_axi_arlock  (axi_ar.lock),
+    .s_axi_arprot  (axi_ar.prot),
+    .s_axi_arqos   (axi_ar.qos),
+    .s_axi_arsize  (axi_ar.size),
+    .s_axi_awready (axi_aw_ready),
+    .s_axi_awvalid (axi_aw_valid),
+    .s_axi_awaddr  (axi_aw.addr),
+    .s_axi_awburst (axi_aw.burst),
+    .s_axi_awcache (axi_aw.cache),
+    .s_axi_awid    (axi_aw.id),
+    .s_axi_awlen   (axi_aw.len),
+    .s_axi_awlock  (axi_aw.lock),
+    .s_axi_awprot  (axi_aw.prot),
+    .s_axi_awqos   (axi_aw.qos),
+    .s_axi_awsize  (axi_aw.size),
+    .s_axi_bready  (axi_b_ready),
+    .s_axi_bvalid  (axi_b_valid),
+    .s_axi_bid     (axi_b.id),
+    .s_axi_bresp   (axi_b.resp),
+    .s_axi_rready  (axi_r_ready),
+    .s_axi_rvalid  (axi_r_valid),
+    .s_axi_rdata   (axi_r.data),
+    .s_axi_rid     (axi_r.id),
+    .s_axi_rlast   (axi_r.last),
+    .s_axi_rresp   (axi_r.resp),
+    .s_axi_wready  (axi_w_ready),
+    .s_axi_wvalid  (axi_w_valid),
+    .s_axi_wdata   (axi_w.data),
+    .s_axi_wlast   (axi_w.last),
+    .s_axi_wstrb   (axi_w.strb),
+    // Other signals
+    .device_temp_i       ('0),
+    .device_temp         (  ),
+    .app_sr_req          ('0),
+    .app_sr_active       (  ),
+    .app_ref_req         ('0),
+    .app_ref_ack         (  ),
+    .app_zq_req          ('0),
+    .app_zq_ack          (  ),
+    .init_calib_complete (  )
+  );
+
+  // Clock converter
+
+  `AXI_DECLARE(DataWidth, AddrWidth, SourceWidth, axi_sync);
+
+  axi_clock_converter_ddr clock_cvt (
+    .s_axi_aclk     (clk_o),
+    .s_axi_aresetn  (rst_no),
+    .s_axi_arready  (axi_sync_ar_ready),
+    .s_axi_arvalid  (axi_sync_ar_valid),
+    .s_axi_araddr   (axi_sync_ar.addr),
+    .s_axi_arburst  (axi_sync_ar.burst),
+    .s_axi_arcache  (axi_sync_ar.cache),
+    .s_axi_arid     (axi_sync_ar.id),
+    .s_axi_arlen    (axi_sync_ar.len),
+    .s_axi_arlock   (axi_sync_ar.lock),
+    .s_axi_arprot   (axi_sync_ar.prot),
+    .s_axi_arqos    (axi_sync_ar.qos),
+    .s_axi_arregion (axi_sync_ar.region),
+    .s_axi_arsize   (axi_sync_ar.size),
+    .s_axi_awready  (axi_sync_aw_ready),
+    .s_axi_awvalid  (axi_sync_aw_valid),
+    .s_axi_awaddr   (axi_sync_aw.addr),
+    .s_axi_awburst  (axi_sync_aw.burst),
+    .s_axi_awcache  (axi_sync_aw.cache),
+    .s_axi_awid     (axi_sync_aw.id),
+    .s_axi_awlen    (axi_sync_aw.len),
+    .s_axi_awlock   (axi_sync_aw.lock),
+    .s_axi_awprot   (axi_sync_aw.prot),
+    .s_axi_awqos    (axi_sync_aw.qos),
+    .s_axi_awregion (axi_sync_aw.region),
+    .s_axi_awsize   (axi_sync_aw.size),
+    .s_axi_bready   (axi_sync_b_ready),
+    .s_axi_bvalid   (axi_sync_b_valid),
+    .s_axi_bid      (axi_sync_b.id),
+    .s_axi_bresp    (axi_sync_b.resp),
+    .s_axi_rready   (axi_sync_r_ready),
+    .s_axi_rvalid   (axi_sync_r_valid),
+    .s_axi_rdata    (axi_sync_r.data),
+    .s_axi_rid      (axi_sync_r.id),
+    .s_axi_rlast    (axi_sync_r.last),
+    .s_axi_rresp    (axi_sync_r.resp),
+    .s_axi_wready   (axi_sync_w_ready),
+    .s_axi_wvalid   (axi_sync_w_valid),
+    .s_axi_wdata    (axi_sync_w.data),
+    .s_axi_wlast    (axi_sync_w.last),
+    .s_axi_wstrb    (axi_sync_w.strb),
+    .m_axi_aclk     (mig_clk),
+    .m_axi_aresetn  (rst_no),
+    .m_axi_arready  (axi_ar_ready),
+    .m_axi_arvalid  (axi_ar_valid),
+    .m_axi_araddr   (axi_ar.addr),
+    .m_axi_arburst  (axi_ar.burst),
+    .m_axi_arcache  (axi_ar.cache),
+    .m_axi_arid     (axi_ar.id),
+    .m_axi_arlen    (axi_ar.len),
+    .m_axi_arlock   (axi_ar.lock),
+    .m_axi_arprot   (axi_ar.prot),
+    .m_axi_arqos    (axi_ar.qos),
+    .m_axi_arregion (axi_ar.region),
+    .m_axi_arsize   (axi_ar.size),
+    .m_axi_awready  (axi_aw_ready),
+    .m_axi_awvalid  (axi_aw_valid),
+    .m_axi_awaddr   (axi_aw.addr),
+    .m_axi_awburst  (axi_aw.burst),
+    .m_axi_awcache  (axi_aw.cache),
+    .m_axi_awid     (axi_aw.id),
+    .m_axi_awlen    (axi_aw.len),
+    .m_axi_awlock   (axi_aw.lock),
+    .m_axi_awprot   (axi_aw.prot),
+    .m_axi_awqos    (axi_aw.qos),
+    .m_axi_awregion (axi_aw.region),
+    .m_axi_awsize   (axi_aw.size),
+    .m_axi_bready   (axi_b_ready),
+    .m_axi_bvalid   (axi_b_valid),
+    .m_axi_bid      (axi_b.id),
+    .m_axi_bresp    (axi_b.resp),
+    .m_axi_rready   (axi_r_ready),
+    .m_axi_rvalid   (axi_r_valid),
+    .m_axi_rdata    (axi_r.data),
+    .m_axi_rid      (axi_r.id),
+    .m_axi_rlast    (axi_r.last),
+    .m_axi_rresp    (axi_r.resp),
+    .m_axi_wready   (axi_w_ready),
+    .m_axi_wvalid   (axi_w_valid),
+    .m_axi_wdata    (axi_w.data),
+    .m_axi_wlast    (axi_w.last),
+    .m_axi_wstrb    (axi_w.strb)
+  );
+
+  // TileLink to AXI bridge
+
+  tl_axi_adapter #(
+    .DataWidth (DataWidth),
+    .AddrWidth (AddrWidth),
+    .SourceWidth (SourceWidth),
+    .IdWidth (SourceWidth)
+  ) adapter (
+    .clk_i (clk_o),
+    .rst_ni (rst_no),
+    `TL_FORWARD_DEVICE_PORT(host, link),
+    `AXI_CONNECT_HOST_PORT(device, axi_sync)
+  );
+
+endmodule
